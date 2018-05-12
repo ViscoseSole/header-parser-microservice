69bafd4bf17de77d99b1081a931529face727402
ViscoseSole <n.hulttinen97@gmail.com>
2018-05-12 16:10:10 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const
+  express = require('express'),
+
+  app = express(),
+  port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send("<b> Find your IP address, System Language and Operating System at </b> <a href='whoami'> /whoami </a>");
+});
+
+app.get('/whoami', (req, res) => {
+  let ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+  let language = req.headers["accept-language"].split(",")[0];
+  let software = req.headers["user-agent"].match(/\(.*?\)/)[0];
+
+  res.send({ipaddress: ip, language, software});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
